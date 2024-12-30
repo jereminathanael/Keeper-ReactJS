c312bc9a30010f88dbbea0f247cac753e79c87a4
jereminathanael <jereminathanael332@gmail.com>
2024-12-30 21:02:17 +0700

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function addNote(note) {
+    setItems((prevItems) => {
+      return [...prevItems, note];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea notes={addNote} />
+      {items.map((noteItem, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onChecked={deleteItem}
+        />
+      ))}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+
+function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setNote((prevValue) => {
+      if (name === "title") {
+        return {
+          title: value,
+          content: prevValue.content,
+        };
+      } else if (name === "content") {
+        return {
+          title: prevValue.title,
+          content: value,
+        };
+      }
+    });
+  }
+
+  function expanded() {
+    setExpanded(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded ? (
+          <input
+            onChange={handleChange}
+            name="title"
+            placeholder="Title"
+            value={note.title}
+          />
+        ) : null}
+        <textarea
+          onChange={handleChange}
+          onClick={expanded}
+          name="content"
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+          value={note.content}
+        />
+        <Zoom in={isExpanded}>
+          <Fab
+            onClick={(e) => {
+              props.notes(note);
+              setNote({ title: "", content: "" });
+              e.preventDefault();
+            }}
+          >
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
